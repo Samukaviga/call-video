f2f2ca5d0ddfc167c0aaaf49681bae6067eab315
Samuel Gomes <samuelgomes2021@gmail.com>
2023-10-01 18:02:29 -0300

    Adicionando

---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+const port = process.env.PORT || 4000;
+//const {v4:uuidv4} = require('uuid');
+const {ExpressPeerServer} = require('peer')
+const peer = ExpressPeerServer(server , {
+  debug:true
+});
+
+
+app.use('/peerjs', peer);
+//app.set('view engine', 'ejs')
+app.use(express.static('public'))
+
+/*
+app.get('/' , (req,res)=>{
+  
+
+}); */
+
+app.get('/' , (req,res)=>{ //pega o parametro passado na URL
+   
+  //res.render('/sala' , {RoomId:req.params.room})
+  const room = "sala";
+
+  //console.log(req.params.room);
+  // res.render('index' , {RoomId:req.params.room}); //guarda o nome na variavel RoomId 
+});
+
+
+io.on("connection" , (socket)=>{
+  
+  socket.on('newUser' , (id , room)=>{ //Envia a solicitacao do ID e SALA e recebe
+    
+    socket.join(room); //cria a sala e conecta a ela
+    socket.to(room).broadcast.emit('userJoined' , id); //emite para todos clientes da sala menos para o conectado
+    
+
+    socket.on('disconnect' , ()=>{ //caso o usuario se desconect
+        socket.to(room).broadcast.emit('userDisconnect' , id);
+    })
+  })
+})
+server.listen(port , ()=>{
+  console.log("Server running on port : " + port);
+})
